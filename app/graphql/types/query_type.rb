1a21b95c1b9b856b151613fa9c3073adc9553b67
arishavelle18 <arishavelle18@yahoo.com>
2023-05-09 04:37:01 +0800

    add fetch gender interest

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -13,5 +13,18 @@ module Types
     def user(id:)
       User.find(id)
     end
+
+    field :male_users, [Types::UserType], null: false, description: "Returns all male users"
+    field :female_users, [Types::UserType], null: false, description: "Returns all female users"
+
+    def male_users
+
+      User.where(gender: true).where.not(id: context[:current_user].id)
+    end
+
+    def female_users
+      User.where(gender: false).where.not(id: context[:current_user])
+    end
+
   end
 end
